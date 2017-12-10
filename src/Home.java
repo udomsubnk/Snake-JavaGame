adbed96e43e526c6bf8a5c2decf14fbf5b4b0275
Udomsub Naikorn <udomsub.nk@gmail.com>
2017-12-10 16:49:56 +0700

    Add Home.java & Main.java

---
+++ src/Home.java
@@ -0,0 +1,26 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import javax.swing.*;
+import java.awt.*;
+import javax.swing.border.TitledBorder;
+
+public class Home extends JFrame implements ActionListener {
+	public Home(){
+	
+	}
+	protected void paintComponent(Graphics f){
+		super.paintComponents(f);
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+}
+
+
+
+	
+
